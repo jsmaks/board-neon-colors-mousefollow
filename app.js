bd5b1c0090940e63e82639b74c610334fdfdef72
jsmaks <jsmaks26@gmail.com>
2022-01-15 18:41:31 +0200

    neon

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board');
+colors = ['#e74c3c', '#8e44ad', '#F0FFFF', '#e67e22', '#2ecc71', '#FF1493', '#0000FF', '#FF0000'];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => {
+    setColor(square);
+  });
+  square.addEventListener('mouseleave', () => {
+    removeColor(square);
+  });
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+ 
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color},  0 0 20px ${color}`;
+//   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color},  0 0 20px ${color}, 0 0 80px ${color}`;
+}
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
